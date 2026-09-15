38555bbe13d254498c2d04cd59322b90206b2001
ahmad lutfy <ahmadlutfy577@gmail.com>
2026-09-15 03:28:34 +0300

    fix deployment file bug

--- deployement/check_health.sh
+++ deployement/check_health.sh
@@ -1,12 +1,12 @@
 
 new_images=$1
-container_number=$(cat /container_info/container_number);
+container_number=$(cat container_info/container_number);
 
 if [ -z "$new_images" ]; then
         echo "there is no image"
         exit 1;
 fi
-echo "=====================================================================================";
+echo "=====================================================================================" >> logfile;
 date >>logfile
 if docker pull "$new_images" >> logfile 2>&1; then
 	echo "the image $new_images is download" >> logfile
@@ -15,25 +15,28 @@ else
 	exit 1
 fi
 
-echo "=>  run a test container" >> logfile; 
-new_container_id=$(docker run --name to-do-app-test -d -p 3000:3000 $new_images >> logfile 2>&1);
+echo "=>  run a test container" >> logfile;
+echo "### docker run --name to-do-app-test -d -p 3000:3000 $new_images" >>logfile
+new_container_id=$(docker run --name to-do-app-test -d -p 3000:3030 $new_images 2>&1);
+echo "new_container_id =$new_container_id" >>logfile
 echo "=>  check health the test container" >> logfile;
 for ((i =0 ; i < 12 ; i++)) do
 	if curl -fsS http://localhost:3000/health >>logfile ;then
-		echo ""
+		echo "">> logfile
 		echo "=>  the container is work" >> logfile
 
 		break;
 	else
-	
 		echo "=>  the container not work" >> logfile
 		if [ "$i" -eq 11 ]; then
 			echo "=>  the container is not healty" >> logfile;
 
-			echo "=>  stop the test container" >> logfile 
+			echo "=>  stop the test container" >> logfile
+		       	echo "### docker stop to-do-app-test"	
 			docker stop to-do-app-test >> logfile 2>&1|| true ;
 
 			echo "=>  remove the test container" >>logfile;
+			echo "### docker rm to-do-app-test"
 			docker rm to-do-app-test >>logfile 2>&1|| true 
 			
 			echo  "=>  exit the script" >>logfile
@@ -43,30 +46,47 @@ for ((i =0 ; i < 12 ; i++)) do
 sleep 2
 done;
 
-echo "=>  stop the test-container" >>logfile
+echo "=>  stop the tet-container" >>logfile
+echo "### docker stop $new_container_id" >>logfile
 docker stop $new_container_id >>logfile 2>&1 || true 
 
 echo "=>  remove the test-conatiner">> logfile
+echo "### docker rm $new_container_id" >>logfile
 docker rm $new_container_id >>logfile 2>&1|| true 
 
-echo "=> run a prodation container from the new image"
+echo "=> remove the old prodation container" >> logfile;
+old_container_id=$(cat container_info/old_container_id);
+echo "old_container_id =$old_container_id " >> logfile;
+if [ -n old_container_id ]; then
+	echo "=> stop the old container" >> logfile
+	echo "docker stop $old_container_id" >> logfile
+	docker stop $old_container_id >> logfile 2>&1 || true
 
+	echo "rm the old container" >> logfile
+	echo "docker rm $old_container_id" >> logfile
+	docker rm $old_container_id >> logfile 2>&1 || true
+fi
+echo "=> run a prodation container from the new image" >>logfile
 
-new_port=$(cat /container_info/new_port)
+echo "### new_port cat container_info/newport" >>logfile
+new_port=$(cat container_info/new_port);
 if [ $new_port -eq 80 ]; then
 	new_port=81;
 else
 	new_port=80;
 fi
+echo "### container_number++" >>logfile
 ((container_number++))
-prodaction_container_id=$(docker run -d -p $new_port:3000 --name prodaction-to-do-app-$container_number $new_images >> logfile 2>&1)
-
+echo "new_port =$new_port container_number=$container_number" >>logfile
+echo "### docker rum -d -p $new_port:3030 --name prodaction-to-do-app-$container_number $new_images" >> logfile
+prodaction_container_id=$(docker run -d -p $new_port:3000 --name prodaction-to-do-app-$container_number $new_images 2>&1)
+echo "prodacation_container_id =$prodaction_container_id" >> logfile
 
 echo "=>  test the prodaction container" >> logfile;
 
 for ((i =0 ; i < 12 ; i++)) do
 	if curl -fsS http://localhost:$new_port/health >>logfile ;then
-		echo ""
+		echo "" >> logfile
 		echo "=>  the container is healthy" >> logfile
 
 		break;
@@ -91,25 +111,18 @@ for ((i =0 ; i < 12 ; i++)) do
 sleep 2
 done;
 echo "=>  update the state" >>logfile
-echo "$prodaction_container_id" > /container_info/new_container_id;
-echo "$conatiner_number" > /container_info/container_number;
-echo "$new_port" > /container_info/new_port;
+echo "=====test the variable==========" >> logfile
+echo "prodaction conatiner id =$prodaction_container_id" >>logfile
+echo "continaer number = $conatiner_number" >> logfile
+echo "new port $new_port" >> logfile
+echo "$prodaction_container_id" > container_info/new_container_id;
+echo "$container_number" > container_info/container_number;
+echo "$new_port" > container_info/new_port;
 
-
-echo "=>  remove the old prodaction container" >>logfile;
-old_container_id=$(cat /container_info/old_container_id );
-if [ -n "$old_container_id" ]; then
-
-echo "=>  stop old container">>logfile
-docker stop "$old_container_id" >>logfile 2>&1|| true
-
-echo "=>  rm the old container" >> logfile
-docker rm "$old_container_id" >>logfile 2>&1|| true 
-fi
 echo "=>  change the postion of containers" >> logfile;
 
-cat /container_info/current_container_id > /container_info/old_container_id;
-cat /container_info/new_container_id > /container_info/current_container_id;
+cat container_info/current_container_id > container_info/old_container_id;
+cat container_info/new_container_id > container_info/current_container_id;
 
 
 echo "============ the deployement is end ======================" >> logfile
