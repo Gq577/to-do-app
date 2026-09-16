5f4082f4d299a16eacb3d8a1c6444fcea45c6512
ahmad lutfy <ahmadlutfy577@gmail.com>
2026-09-16 21:39:30 +0300

    add nginx in the deployement

--- deployement/deployment.sh
+++ deployement/deployment.sh
@@ -16,8 +16,8 @@ else
 fi
 
 echo "=>  run a test container" >> logfile;
-echo "### docker run --name to-do-app-test -d -p 3000:3000 $new_images" >>logfile
-new_container_id=$(docker run --name to-do-app-test -d -p 3000:3030 $new_images 2>&1);
+echo "### docker run --name to-do-app-test -d -p 3000:3000  $new_images" >>logfile
+new_container_id=$(docker run --name to-do-app-test -d -p 3000:3000 $new_images 2>&1);
 echo "new_container_id =$new_container_id" >>logfile
 echo "=>  check health the test container" >> logfile;
 for ((i =0 ; i < 12 ; i++)) do
@@ -57,7 +57,7 @@ docker rm $new_container_id >>logfile 2>&1|| true
 echo "=> remove the old prodation container" >> logfile;
 old_container_id=$(cat container_info/old_container_id);
 echo "old_container_id =$old_container_id " >> logfile;
-if [ -n old_container_id ]; then
+if [ -n "$old_container_id" ]; then
 	echo "=> stop the old container" >> logfile
 	echo "docker stop $old_container_id" >> logfile
 	docker stop $old_container_id >> logfile 2>&1 || true
@@ -75,11 +75,9 @@ if [ $new_port -eq 80 ]; then
 else
 	new_port=80;
 fi
-echo "### container_number++" >>logfile
 ((container_number++))
-echo "new_port =$new_port container_number=$container_number" >>logfile
-echo "### docker rum -d -p $new_port:3030 --name prodaction-to-do-app-$container_number $new_images" >> logfile
-prodaction_container_id=$(docker run -d -p $new_port:3000 --name prodaction-to-do-app-$container_number $new_images 2>&1)
+echo "### docker rum -d -p $new_port:3000 --name prodaction-to-do-app-$container_number $new_images" >> logfile
+prodaction_container_id=$(docker run -d -p $new_port:3000 --name --network app-network prodaction-to-do-app-$container_number $new_images 2>&1)
 echo "prodacation_container_id =$prodaction_container_id" >> logfile
 
 echo "=>  test the prodaction container" >> logfile;
@@ -102,18 +100,52 @@ for ((i =0 ; i < 12 ; i++)) do
 			echo "=>  remove the prodaction container" >>logfile;
 			docker rm $prodaction_container_id >>logfile 2>&1|| true 
 			
-			echo "=>  delete the new image" >> logfile
-			docker rmi $new_images
 			echo  "=>  exit the script" >>logfile
 			exit 1;
 		fi
 	fi
 sleep 2
 done;
+cat nginx/nginx.conf > container_info/old_nginx_script
+	echo "server {
+    listen 83;
+
+    location / {
+        proxy_pass http://prodaction-to-do-app-$container_number:3000;
+    }
+	}" > nginx/nginx.conf
+if docker exec nginx nginx -t >> logfile 2>&1; then
+
+    if docker exec nginx nginx -s reload >> logfile 2>&1; then
+        echo "=> nginx reload successful" >> logfile
+
+    else
+        echo "=> nginx reload failed, rollback nginx config" >> logfile
+
+        cat container_info/old_nginx_script > nginx/nginx.conf
+
+        if docker exec nginx nginx -t >> logfile 2>&1; then
+            docker exec nginx nginx -s reload >> logfile 2>&1
+            echo "=> nginx rollback successful" >> logfile
+        else
+            echo "=> nginx rollback config is invalid" >> logfile
+        fi
+
+        exit 1
+    fi
+
+else
+    echo "=> nginx configuration is invalid, rollback nginx config" >> logfile
+
+    cat container_info/old_nginx_script > nginx/nginx.conf
+
+    exit 1
+fi
+
 echo "=>  update the state" >>logfile
 echo "=====test the variable==========" >> logfile
 echo "prodaction conatiner id =$prodaction_container_id" >>logfile
-echo "continaer number = $conatiner_number" >> logfile
+echo "continaer number = $container_number" >> logfile
 echo "new port $new_port" >> logfile
 echo "$prodaction_container_id" > container_info/new_container_id;
 echo "$container_number" > container_info/container_number;
@@ -126,3 +158,4 @@ cat container_info/new_container_id > container_info/current_container_id;
 
 
 echo "============ the deployement is end ======================" >> logfile
+
